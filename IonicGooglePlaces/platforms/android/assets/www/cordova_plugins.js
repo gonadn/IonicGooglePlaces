cd635ebba79d4605d35e17ca346914706f0f9446
Adnan Riaz Gondal <gonadn@live.no>
2015-11-05 20:08:45 +0100

    ...

---
+++ IonicGooglePlaces/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,41 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-keyboard/www/keyboard.js",
+        "id": "cordova-plugin-keyboard.keyboard",
+        "clobbers": [
+            "window.Keyboard"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.console": "0.2.13",
+    "org.apache.cordova.device": "0.3.0",
+    "org.apache.cordova.splashscreen": "1.0.0",
+    "org.apache.cordova.geolocation": "0.3.12",
+    "cordova-plugin-whitelist": "1.1.1-dev",
+    "cordova-plugin-keyboard": "1.1.2"
+}
+// BOTTOM OF METADATA
+});
